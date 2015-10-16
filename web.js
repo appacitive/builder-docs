e30b5bdc9deb7135c5da62a1e1b4a41b5d7bdfe2
Chirag Sanghvi <csanghvi@appacitive.com>
2015-10-16 12:28:29 +0530

    Changed web.js to avoid harp error

--- web.js
+++ web.js
@@ -27,34 +27,30 @@ setTimeout(function() {
     console.log("Compiling html using Harp");
 
     exec('harp compile server publish', function(err, stdout, stderr) {
-        if (!err) {
+        
+        console.log("Html compiled\n\n");
+        var version = guid();
+        var transform = function(content, filename) {
+            console.log("Transforming " + filename);
 
-            console.log("Html compiled\n\n");
-            var version = guid();
-            var transform = function(content, filename) {
-                console.log("Transforming " + filename);
+            content = replaceAll('__RevisionNoGoesHere__', version, content);
 
-                content = replaceAll('__RevisionNoGoesHere__', version, content);
-
-                require('fs').writeFile(path.resolve(filename), content, function(err) {
-                    if (err) throw err;
-                    console.log(filename + " saved");
-                });
-            };
-
-            require('node-dir').readFiles(__dirname + '/publish', {
-                match: /.html$/
-            }, function(err, content, filename, next) {
+            require('fs').writeFile(path.resolve(filename), content, function(err) {
                 if (err) throw err;
-                transform(content, filename);
-                next();
-            }, function(err, files) {
-                if (err) throw err;
-                console.log('finished transforming files');
+                console.log(filename + " saved");
             });
-        } else {
-             throw new Error("Couldn't compile html using harp");
-        }
+        };
+
+        require('node-dir').readFiles(__dirname + '/publish', {
+            match: /.html$/
+        }, function(err, content, filename, next) {
+            if (err) throw err;
+            transform(content, filename);
+            next();
+        }, function(err, files) {
+            if (err) throw err;
+            console.log('finished transforming files');
+        });
     });
 }, 0);
 
